7fc5e3f4aaaf3a547e29ace7b6c635a81303ecd7
rocopolas <86751781+rocopolas@users.noreply.github.com>
2025-09-04 19:26:28 -0300

    Add files via upload

--- config/db_connection.php
+++ config/db_connection.php
@@ -1,7 +1,7 @@
 <?php
 // Conexión a la base de datos
 function getConnection() {
-    $conexion = new mysqli("192.168.101.93", "ag01", "StudentAG01_2025", "ag01", 3306);
+    $conexion = new mysqli("localhost", "root", "", "nes");
     if ($conexion->connect_error) {
         die("Error de conexión: " . $conexion->connect_error);
     }
